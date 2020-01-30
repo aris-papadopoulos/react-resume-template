a3bcdf376b6201500ef2363f072e3cc2a00b5108
Aris Papadopoulos <aris.ppd@gmail.com>
2020-01-30 16:31:22 +0200

    Info: Adds Info component
    Signed-off-by: Aris Papadopoulos <aris.ppd@gmail.com>

---
+++ src/Info.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+function Info() {
+  return (
+    <div className="info">
+        <h2>About me</h2>
+        <p>Prolific, full stack web developer with a passion for metrics and beating former "best-yets." Prototyped 25 new product features per year for Flexor, Inc. Decreased rework by 22% and costs by 15%. Consistently receive high user experience scores for all web development projects, including a 55% increase for Flexor, Inc. Passionate about building world class web applications. One of my sites received a 2015 Webby for Best Navigation and Structure.</p>
+    </div>
+  );
+}
+
+export default Info;
