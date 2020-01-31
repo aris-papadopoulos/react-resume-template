66a88d4d276d787bb68a0a7cdc949139ddfa139f
Aris Papadopoulos <aris.ppd@gmail.com>
2020-01-31 13:09:16 +0200

    Info: Changes styling
    Signed-off-by: Aris Papadopoulos <aris.ppd@gmail.com>

--- src/Info.js
+++ src/Info.js
@@ -7,7 +7,7 @@ class Info extends React.Component {
 
     renderView({ style, ...props }) {
         const viewStyle = {
-            padding: 15
+            padding: '0 15px'
         };
         return (
             <div
@@ -16,11 +16,12 @@ class Info extends React.Component {
                 {...props}/>
         );
     }
+
     render() {
         return (
             <div className="info">
                 <Scrollbars renderView={this.renderView}>
-                    <h2>About me</h2>
+                    <h3>About me</h3>
                     <p>Prolific, full stack web developer with a passion for metrics and beating former "best-yets." Prototyped 25 new product features per year for Flexor, Inc. Decreased rework by 22% and costs by 15%. Consistently receive high user experience scores for all web development projects, including a 55% increase for Flexor, Inc. Passionate about building world class web applications. One of my sites received a 2015 Webby for Best Navigation and Structure.</p>
                     <p>
                         Today, we live and breathe media, minute-by-minute, hour-by-hour. News, television, social media even recognize.
