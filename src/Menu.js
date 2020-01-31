88d4d374fd23e2003695baec56fbbd26c5eb1410
Aris Papadopoulos <aris.ppd@gmail.com>
2020-01-31 13:48:41 +0200

    Menu: Adds state for switching between menu items
    Signed-off-by: Aris Papadopoulos <aris.ppd@gmail.com>

--- src/Menu.js
+++ src/Menu.js
@@ -1,21 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { FaUser } from "react-icons/fa";
-import { FaPhone } from 'react-icons/fa';
-import { FaRegEdit } from 'react-icons/fa';
 import { FaBriefcase } from 'react-icons/fa';
+import { FaCoffee } from 'react-icons/fa';
+import { FaPhone } from 'react-icons/fa';
 
 
-function Menu() {
-  return (
-    <div className="menu">
-        <ul>
-            <li><FaUser /></li>
-            <li><FaPhone /></li>
-            <li><FaRegEdit /></li>
-            <li><FaBriefcase /></li>
-        </ul>
-    </div>
-  );
+/**
+*   @author Aris Papadopoulos <aris.ppd@gmail.com> 
+*   Site menu, selects page to be displayed in "info" component
+*/
+const Menu = () => {
+
+    const [selected, setSelected] = useState(0);
+
+    function changeMenu(i) {
+        setSelected(i);
+    }
+
+    return (
+        <div className="menu">
+            <ul>
+                <li onClick={() => changeMenu('main')} className={(selected === 'main') ? 'selected' : ''}><FaUser /></li>
+                <li onClick={() => changeMenu('work-samples')} className={(selected === 'work-samples') ? 'selected' : ''}><FaBriefcase /></li>
+                <li onClick={() => changeMenu('articles')} className={(selected === 'articles') ? 'selected' : ''}><FaRegEdit /></li>
+                <li onClick={() => changeMenu('interests')} className={(selected === 'work-samples') ? 'selected' : ''}><FaCoffee /></li>
+                <li onClick={() => changeMenu('contact')} className={(selected === 'contact') ? 'selected' : ''}><FaPhone /></li>
+            </ul>
+        </div>
+    );
 }
+
 
 export default Menu;
