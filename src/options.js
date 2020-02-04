8526169e446f6780b1e29aaf107ad1c97141c9bf
Aris Papadopoulos <aris.ppd@gmail.com>
2020-02-04 15:56:41 +0200

    Creates new "options" file including all template option variables
    Signed-off-by: Aris Papadopoulos <aris.ppd@gmail.com>

---
+++ src/options.js
@@ -0,0 +1,15 @@
+/* Change the theme's main color - can be either 'blue', 'red' or 'green' */
+export const theme_color = 'blue'; 
+/* Set to "true" to enable or "false" to disable sections of the template */ 
+export const sections = {
+    basic_info: true,
+    contact: true,
+    work_samples: true,
+    articles: true,
+    interests: true
+}
+/* The Sheet ID containing your site's data. The ID can be found on the URL of the sheet */
+export const GOOGLE_SHEET_ID = '1raPYKhL5Bk0y3H3ti7o4bvGktzGfMW99nMNTXYK-idE';
+/* Your API key, needed for requesting data from Google Sheets. 
+   Can be retrieved from https://developers.google.com/sheets/api/quickstart/js */
+export const GOOGLE_API_KEY = 'AIzaSyBhiqVypmyLHYPmqZYtvdSvxEopcLZBdYU';
