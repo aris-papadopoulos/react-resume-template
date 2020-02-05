df7364ad027fe58a7d7f4c050b3765de8a530e96
Aris Papadopoulos <aris.ppd@gmail.com>
2020-02-05 14:45:22 +0200

    Menu: Changes range of "main" page, removes "interests" page
    Signed-off-by: Aris Papadopoulos <aris.ppd@gmail.com>

--- src/Menu.js
+++ src/Menu.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { FaUser } from "react-icons/fa";
 import { FaBriefcase } from 'react-icons/fa';
 import { FaRegEdit } from 'react-icons/fa';
-import { FaCoffee } from 'react-icons/fa';
 import { FaPhone } from 'react-icons/fa';
 
 
@@ -15,10 +14,9 @@ const Menu = (props) => {
     return (
         <div className="menu">
             <ul>
-                <li onClick={() => props.changeMenu({sheetTitle: 'main', range: 'A:B'})} className={(props.selected.sheetTitle === 'main') ? 'active' : ''}><FaUser /></li>
+                <li onClick={() => props.changeMenu({sheetTitle: 'main', range: 'A:D'})} className={(props.selected.sheetTitle === 'main') ? 'active' : ''}><FaUser /></li>
                 <li onClick={() => props.changeMenu({sheetTitle: 'work-samples', range: 'A:C'})} className={(props.selected.sheetTitle === 'work-samples') ? 'active' : ''}><FaBriefcase /></li>
                 <li onClick={() => props.changeMenu({sheetTitle: 'articles', range: 'A:C'})} className={(props.selected.sheetTitle === 'articles') ? 'active' : ''}><FaRegEdit /></li>
-                <li onClick={() => props.changeMenu({sheetTitle: 'interests', range: 'A:B'})} className={(props.selected.sheetTitle === 'interests') ? 'active' : ''}><FaCoffee /></li>
                 <li onClick={() => props.changeMenu({sheetTitle: 'contact', range: 'A:B'})} className={(props.selected.sheetTitle === 'contact') ? 'active' : ''}><FaPhone /></li>
             </ul>
         </div>
