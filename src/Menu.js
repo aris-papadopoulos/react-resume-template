f46712d74e684697cfa3d125bf1ca19590c90915
Aris Papadopoulos <aris.ppd@gmail.com>
2020-02-06 10:49:50 +0200

    Menu: Removes "artlcles" page and adds "skill-set" page
    Signed-off-by: Aris Papadopoulos <aris.ppd@gmail.com>

--- src/Menu.js
+++ src/Menu.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { FaUser } from "react-icons/fa";
+import { FaStar } from 'react-icons/fa';
 import { FaBriefcase } from 'react-icons/fa';
-import { FaRegEdit } from 'react-icons/fa';
 import { FaPhone } from 'react-icons/fa';
 
 
@@ -15,8 +15,8 @@ const Menu = (props) => {
         <div className="menu">
             <ul>
                 <li onClick={() => props.changeMenu({sheetTitle: 'main', range: 'A:D'})} className={(props.selected.sheetTitle === 'main') ? 'active' : ''}><FaUser /></li>
+                <li onClick={() => props.changeMenu({sheetTitle: 'skill-set', range: 'A:B'})} className={(props.selected.sheetTitle === 'skill-set') ? 'active' : ''}><FaStar /></li>
                 <li onClick={() => props.changeMenu({sheetTitle: 'work-samples', range: 'A:C'})} className={(props.selected.sheetTitle === 'work-samples') ? 'active' : ''}><FaBriefcase /></li>
-                <li onClick={() => props.changeMenu({sheetTitle: 'articles', range: 'A:C'})} className={(props.selected.sheetTitle === 'articles') ? 'active' : ''}><FaRegEdit /></li>
                 <li onClick={() => props.changeMenu({sheetTitle: 'contact', range: 'A:B'})} className={(props.selected.sheetTitle === 'contact') ? 'active' : ''}><FaPhone /></li>
             </ul>
         </div>
--- src/options.js
+++ src/options.js
@@ -3,10 +3,9 @@ export const theme_color = 'blue';
 /* Set to "true" to enable or "false" to disable sections of the template */ 
 export const sections = {
     basic_info: true,
-    contact: true,
+    skill_set: true,
     work_samples: true,
-    articles: true,
-    interests: true
+    contact: true
 }
 /* The Sheet ID containing your site's data. The ID can be found on the URL of the sheet */
 export const GOOGLE_SHEET_ID = '1raPYKhL5Bk0y3H3ti7o4bvGktzGfMW99nMNTXYK-idE';
