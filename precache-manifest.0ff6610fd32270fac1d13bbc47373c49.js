4b1f7c5e1d67e42c807389e84c77d6970a0f01d5
Aris Papadopoulos <aris.ppd@gmail.com>
2020-02-14 17:56:29 +0200

    Updates

---
+++ precache-manifest.0ff6610fd32270fac1d13bbc47373c49.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9868ef89c0c38c2a56b997eb88310fbb",
+    "url": "/react-resume-template/index.html"
+  },
+  {
+    "revision": "cb7f2b205fcf34af2a98",
+    "url": "/react-resume-template/static/css/main.d4a25250.chunk.css"
+  },
+  {
+    "revision": "b78bd6bfbbda56368261",
+    "url": "/react-resume-template/static/js/2.4c463edb.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/react-resume-template/static/js/2.4c463edb.chunk.js.LICENSE"
+  },
+  {
+    "revision": "cb7f2b205fcf34af2a98",
+    "url": "/react-resume-template/static/js/main.6728b63c.chunk.js"
+  },
+  {
+    "revision": "35906921750a6d31c6c8",
+    "url": "/react-resume-template/static/js/runtime-main.228f9154.js"
+  }
+]);
